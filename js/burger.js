22c2897b6b2466d6f65c11dd9e9c559cb8c0f003
Gazman4iK1 <133883724+Gazman4iK1@users.noreply.github.com>
2023-11-24 17:37:19 +0200

    Delete js directory

--- js/burger.js
+++
@@ -1,9 +0,0 @@
-let headB = document.querySelector(".header__burger");
-let headM = document.querySelector(".header__menu");
-
-
-document.querySelector(".header__burger").onclick = function () {
-    headB.classList.toggle('active')
-    headM.classList.toggle('active')
-    document.querySelector("body").classList.toggle('lock')
-}
